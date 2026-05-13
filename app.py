a0421ba1c475c16d72427817b618df69a5ff5634
Deeksha Shakyawal <119758705+Deekshashakyawal@users.noreply.github.com>
2026-05-13 15:33:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,11 +16,30 @@ if "ready" not in st.session_state:
 # ----------------------------
 # UPLOAD SECTION (always visible)
 # ----------------------------
-uploaded_file = st.file_uploader("Upload your PDF", type=["pdf"])
+uploaded_file = st.file_uploader("Upload your PDF", type="pdf")
+if "last_uploaded_file" not in st.session_state:
+    st.session_state.last_uploaded_file = None
 if uploaded_file is not None:
     if uploaded_file.name.split(".")[-1].lower() != "pdf":
         st.error("Only PDF files are allowed!")
         st.stop()
+    if uploaded_file.name != st.session_state.last_uploaded_file:
+
+        # reset app for new PDF
+        st.session_state.ready = False
+        st.session_state.history = []
+        st.session_state.last_uploaded_file = uploaded_file.name
+
+        # save file
+        with open("temp.pdf", "wb") as f:
+            f.write(uploaded_file.read())
+
+        # rebuild vector DB
+        with st.spinner("Processing new PDF..."):
+            ingest_pdf("temp.pdf")
+
+        st.session_state.ready = True
+        st.success("PDF ready! Ask questions below 👇")
 if uploaded_file:
     if not st.session_state.ready:
         with open("temp.pdf", "wb") as f:
