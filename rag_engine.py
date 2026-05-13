7c880b57874f8f1b39754ee9aba514a98a055dd2
Deeksha Shakyawal <119758705+Deekshashakyawal@users.noreply.github.com>
2026-05-13 15:21:18 +0530

    Update rag_engine.py

--- rag_engine.py
+++ rag_engine.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
